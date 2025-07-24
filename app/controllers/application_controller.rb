40c01b77e2309ff3e18f08924abf819c18b5416b
Guilherme Ribeiro <devguilhermeribeiro000@gmail.com>
2025-07-24 02:40:32 -0300

    Setup helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate
+    redirect_to login_path, alert: "Nescessário fazer o login" if current_user.nil?
+  end
 end
